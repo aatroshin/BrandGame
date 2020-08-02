6632ff9de75e2466404f5e337f7429b1859fd71d
Arseny Troshin <aatroshin@bk.ru>
2020-08-02 21:04:31 +0500

    Initial Commit

---
+++ GameTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameTwo
+//
+//  Created by Арсений Трошин on 02.08.2020.
+//  Copyright © 2020 Арсений Трошин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
